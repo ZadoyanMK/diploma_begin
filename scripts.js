125498d242bcdf06107110048345bbf01901dc05
ZadoyanMK <konzamir@gmail.com>
2019-02-17 17:10:46 +0200

    add displaying arduino nano and rs485-ttl

--- scripts.js
+++ scripts.js
@@ -1,34 +1,39 @@
+var textBlockData = {
+    '.arduino-nano': {
+        'title': 'Arduino nano',
+        'description': 'arduino nano description'
+    },
+    '.rs485-ttl': {
+        'title': 'rs485-ttl',
+        'description': 'rs485-ttl desc'
+    },
+
+}
+
 $( document ).ready(function() {
-    $('.arduino-nano').tooltip(
-        {
-            delay: { "show": 100, "hide": 100 },
-            title: 'arduino nano', 
-        }
-    )
-    $(".arduino-nano").click(function(e){
-        $('#exampleModalCenter .modal-body').html('test arduino description')
-        $('#exampleModalCenter').modal('show')
-    });
-
-    $('.servo').tooltip(
-        {
-            delay: { "show": 100, "hide": 100 },
-            title: 'SG90 Servo', 
-        }
-    )
-    $(".servo").click(function(e){
-        $('#exampleModalCenter .modal-body').html('test servo description')
-        $('#exampleModalCenter').modal('show')
-    });
-
-    $('.raspberry').tooltip(
-        {
-            delay: { "show": 100, "hide": 100 },
-            title: 'Raspberry Pi2', 
-        }
-    )
-    $(".raspberry").click(function(e){
-        $('#exampleModalCenter .modal-body').html('test raspberry description')
-        $('#exampleModalCenter').modal('show')
-    });
+    [
+        '.arduino-nano', '.rs485-ttl',
+    ].map( (ob) => {
+        $(ob).hover( (e) => {
+            $(ob).css({
+                opacity: 1
+            });
+        })
+        $(ob).mouseover( (e) => {
+            $(ob).css({
+                opacity: 0.7
+            });
+        })
+    
+        $(ob).tooltip(
+            {
+                delay: { "show": 100, "hide": 100 },
+                title: textBlockData[ob]['title'], 
+            }
+        )
+        $(ob).click(function(e){
+            $('#exampleModalCenter .modal-body').html(textBlockData[ob]['description'])
+            $('#exampleModalCenter').modal('show')
+        });
+    })
 });
