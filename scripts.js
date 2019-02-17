330570a8178a76b772b9b69e81f753a7e1f463ed
ZadoyanMK <konzamir@gmail.com>
2019-02-18 01:36:10 +0200

    update air ventilation

--- scripts.js
+++ scripts.js
@@ -9,12 +9,21 @@ var textBlockData = {
         'image': 'nano.png',
         'description': 'rs485-ttl desc'
     },
-
+    '.air-pipe': {
+        'title': 'air pipe',
+        'image': 'nano.png',
+        'description': 'air-pipe desc'
+    },
+    '.dht22': {
+        'title': 'dht22',
+        'image': 'nano.png',
+        'description': 'dht22 desc'
+    },
 }
 
 $( document ).ready(function() {
     [
-        '.arduino-nano', '.rs485-ttl',
+        '.arduino-nano', '.rs485-ttl', '.air-pipe', '.dht22'
     ].map( (ob) => {
         $(ob).hover( (e) => {
             $(ob).css({
