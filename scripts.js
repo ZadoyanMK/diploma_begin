be88813e3164dc186f3b1a0430351d8c11ff6687
ZadoyanMK <konzamir@gmail.com>
2019-05-26 22:00:12 +0300

    add language changing

--- scripts.js
+++ scripts.js
@@ -1,166 +1,418 @@
-var textBlockData = {
+const textBlockData = {
+    'en-lang': {
+        'en': {
+            'title': 'English',
+            'description': ''
+        },
+        'ua': {
+            'title': 'Англійська',
+            'description': ''
+        }
+    },
+    'ua-lang': {
+        'en': {
+            'title': 'Ukrainian',
+            'description': ''
+        },
+        'ua': {
+            'title': 'Українська',
+            'description': ''
+        }
+    },
     'flower': {
-        'title': 'Flower',
-        'description': 'this is flower'
+        'en': {
+            'title': 'Flower',
+            'description': 'This is flower!'
+        },
+        'ua': {
+            'title': 'Квітка',
+            'description': 'Це квітка!'
+        }
     },
     'arduino-nano': {
-        'title': 'Arduino nano',
-        'description': 'Nano desc!'
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'rs485-ttl': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'raspberry': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'rs485-usb': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'wifi': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'ds18b20': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'solar-panel': {
-        'title': 'Solar Panel',
-        'description': 'Solar panel description!'
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'solar-panel-voltage': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'solar-panel-amperage': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'global-voltage': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'global-amperage': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'reley': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'sg90-servo': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'laser-receiver': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'laser': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'window': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'mg135-air': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'pir': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'rfid-rc522': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'door': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'air-pipe': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'ventilation-recuperaptor': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'dht22': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'peltye': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'dht11': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'sun-light': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'white-led-strip': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'rgb-led-strip': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'light-sensor': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
     'garage-door': {
-        'title': '',
-        'description': ''
+        'en': {
+            'title': '',
+            'description': ''
+        },
+        'ua': {
+            'title': '',
+            'description': ''
+        }
     },
 }
 
-$( document ).ready(function() {
+function disable(ob){
+    $(ob).addClass('disabled');
+    $(ob).css({
+        opacity: 0.3
+    });
+}
+
+function activate(ob){
+    $(ob).removeClass('disabled');
+    $(ob).css({
+        opacity: 1
+    });
+}
+
+function generate_field_texts(state) {
+    let lang = 'en';
+    disable('.en-lang');
+    activate('.ua-lang');
+
+    if (state == 2) {
+        lang = 'ua';
+        activate('.en-lang');
+        disable('.ua-lang');
+    }
+
     Object.keys(textBlockData).map( (ob) => {
-        $('.' + ob).hover( (e) => {
-            $('.' + ob).css({
-                opacity: 1
+        console.log($('.' + ob).hasClass('disabled'))
+
+        if ($.inArray(ob, ['en-lang', 'ua-lang']) == -1 ){
+            $('.' + ob).hover( (e) => {
+                $('.' + ob).css({
+                    opacity: 1
+                });
             });
-        });
-        $('.' + ob).mouseover( (e) => {
-            $('.' + ob).css({
-                opacity: 0.6
+            $('.' + ob).mouseover( (e) => {
+                $('.' + ob).css({
+                    opacity: 0.6
+                });
             });
-        });
+        }
+
+        $('.' + ob).attr('data-original-title', textBlockData[ob][lang]['title']);
         $('.' + ob).tooltip(
             {
                 delay: { "show": 100, "hide": 100 },
-                title: textBlockData[ob]['title'], 
+                title: textBlockData[ob][lang]['title'], 
             }
         );
-        $('.' + ob).click(function(e){
-            $('#modalWindow #modalWindowTitle').html(textBlockData[ob]['title']);
-            $('#modalWindow #text-description').html(textBlockData[ob]['description']);
 
-            let type = '.jpg';
-            const png_images = [
-                'solar-panel', 'ds18b20', 'pir', 'flower'
-            ];
-
-            if ($.inArray(ob, png_images) != -1){
-                type = '.png';
-            }
-
-            $('#modalWindow #img-description img').attr({
-                src: 'images/for_modal/' + ob + type,
-                alt: textBlockData[ob]['title'],
-            })
-            $('#modalWindow').modal('show')
-        });
+        if ($.inArray(ob, ['en-lang', 'ua-lang']) == -1){
+            $('.' + ob).click(function(e){
+                $('#modalWindow #modalWindowTitle').html(textBlockData[ob][lang]['title']);
+                $('#modalWindow #text-description').html(textBlockData[ob][lang]['description']);
+    
+                let type = '.jpg';
+                const png_images = [
+                    'solar-panel', 'ds18b20', 'pir', 'flower'
+                ];
+    
+                if ($.inArray(ob, png_images) != -1){
+                    type = '.png';
+                }
+    
+                $('#modalWindow #img-description img').attr({
+                    src: 'images/for_modal/' + ob + type,
+                    alt: textBlockData[ob][lang]['title'],
+                })
+                $('#modalWindow').modal('show')
+            });
+        }
+        
     })
+}
+
+$( document ).ready(function() {
+    generate_field_texts(1);
+
+    $('.ua-lang').click(function (e) {
+        generate_field_texts(2);
+    });
+    
+    $('.en-lang').click(function (e) {
+        generate_field_texts(1);
+    });
 });
