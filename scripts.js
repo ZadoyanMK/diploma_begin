06792eb6b2f9eb73eeba252a0844cb2daf5d2b62
ZadoyanMK <konzamir@gmail.com>
2019-05-26 23:06:53 +0300

    add descriptions

--- scripts.js
+++ scripts.js
@@ -31,71 +31,71 @@ const textBlockData = {
     },
     'arduino-nano': {
         'en': {
-            'title': '',
-            'description': ''
+            'title': 'Arduino nano',
+            'description': "The Arduino Nano is a small, complete, and breadboard-friendly board based on the ATmega328 (Arduino Nano 3.x). It has more or less the same functionality of the Arduino Duemilanove, but in a different package. It lacks only a DC power jack, and works with a Mini-B USB cable instead of a standard one."
         },
         'ua': {
-            'title': '',
-            'description': ''
+            'title': 'Arduino nano',
+            'description': "Arduino Nano — це повнофункціональний мініатюрний пристрій на базі мікроконтролера ATmega328 (Arduino Nano 3.0) або ATmega168 (Arduino Nano 2.x), адаптований для використання з макетної плати. За функціональністю пристрій схожий на Arduino Duemilanove, і відрізняється від нього розмірами, відсутністю роз'єму живлення, а також іншим типом (Mini-B) USB-кабелю. Arduino Nano розроблено і випускається фірмою Gravitech. "
         }
     },
     'rs485-ttl': {
         'en': {
-            'title': '',
-            'description': ''
+            'title': 'RS485-TTL',
+            'description': 'This is a module that allows the TTL interface of the microcontroller to be transferred to the RS485 module. It is generally used for industrial automation.'
         },
         'ua': {
-            'title': '',
-            'description': ''
+            'title': 'RS485-TTL',
+            'description': 'Перехідна плата зі стандарту RS485 на TTL (транзисторно-транзисторна логіка). Використовується як з Arduino, так і в інших проектах.'
         }
     },
     'raspberry': {
         'en': {
-            'title': '',
-            'description': ''
+            'title': 'Raspberry PI',
+            'description': "Several generations of Raspberry Pis have been released. All models feature a Broadcom system on a chip (SoC) with an integrated ARM-compatible central processing unit (CPU) and on-chip graphics processing unit (GPU).            Processor speed ranges from 700 MHz to 1.4 GHz for the Pi 3 Model B+; on-board memory ranges from 256 MB to 1 GB RAM. Secure Digital (SD) cards in MicroSDHC form factor (SDHC on early models) are used to store the operating system and program memory. The boards have one to four USB ports. For video output, HDMI and composite video are supported, with a standard 3.5 mm tip-ring-sleeve jack for audio output. Lower-level output is provided by a number of GPIO pins, which support common protocols like I²C. The B-models have an 8P8C Ethernet port and the Pi 3 and Pi Zero W have on-board Wi-Fi 802.11n and Bluetooth. Prices range from US$5 to $35. "
         },
         'ua': {
-            'title': '',
-            'description': ''
+            'title': 'Raspberry PI',
+            'description': "Raspberry Pi побудований на системі-на-чипі (SoC) Broadcom BCM2835, яка включає в себе процесор ARM із тактовою частотою 700 МГц, графічний процесор VideoCore IV, і 512 чи 256 мегабайтів[3] оперативної пам'яті. Твердий диск відсутній, натомість використовується SD карта. Така апаратна начинка дозволяє відтворювати відео формату H.264 в роздільній здатності 1080p, і запускати комп'ютерні ігри на зразок Quake III Arena. "
         }
     },
     'rs485-usb': {
         'en': {
-            'title': '',
-            'description': ''
+            'title': 'RS485-USB',
+            'description': 'This is a module that allows the USB interface of the microcontroller to be transferred to the RS485 module. It is generally used for industrial automation.'
         },
         'ua': {
-            'title': '',
-            'description': ''
+            'title': 'RS485-USB',
+            'description': 'Перехідна плата зі стандарту RS485 на USB (транзисторно-транзисторна логіка). Використовується як з Arduino, так і в інших проектах.'
         }
     },
     'wifi': {
         'en': {
-            'title': '',
-            'description': ''
+            'title': 'Wi-Fi module',
+            'description': 'WiFi Module is a self contained SOC with integrated TCP/IP protocol stack that can give any microcontroller access to your WiFi network.'
         },
         'ua': {
-            'title': '',
-            'description': ''
+            'title': 'Модуль wi-fi',
+            'description': 'Модуль WiFi є автономним SOC з інтегрованим стеком протоколів TCP / IP, який може надати будь-якому доступу мікроконтролера до вашої мережі WiFi.'
         }
     },
     'ds18b20': {
         'en': {
-            'title': '',
-            'description': ''
+            'title': 'DS18B20',
+            'description': "The DS18B20 is a digital temperature meter with a resolution of 9 to 12 bits and a temperature control alarm function. Control parameters can be set by the user and saved in the non-volatile memory of the sensor."
         },
         'ua': {
-            'title': '',
-            'description': ''
+            'title': 'DS18B20',
+            'description': "DS18B20 це цифровий вимірювач температури, з дозволом перетворення 9 - 12 розрядів і функцією тривожного сигналу контролю за температурою. Параметри контролю можуть бути задані користувачем і збережені в енергонезалежній пам'яті датчика."
         }
     },
     'solar-panel': {
         'en': {
-            'title': '',
+            'title': 'Solar panel',
             'description': ''
         },
         'ua': {
-            'title': '',
+            'title': 'Сонячна панель',
             'description': ''
         }
     },
@@ -159,26 +159,26 @@ const textBlockData = {
             'description': ''
         }
     },
-    'laser-receiver': {
-        'en': {
-            'title': '',
-            'description': ''
-        },
-        'ua': {
-            'title': '',
-            'description': ''
-        }
-    },
-    'laser': {
-        'en': {
-            'title': '',
-            'description': ''
-        },
-        'ua': {
-            'title': '',
-            'description': ''
-        }
-    },
+    // 'laser-receiver': {
+    //     'en': {
+    //         'title': '',
+    //         'description': ''
+    //     },
+    //     'ua': {
+    //         'title': '',
+    //         'description': ''
+    //     }
+    // },
+    // 'laser': {
+    //     'en': {
+    //         'title': '',
+    //         'description': ''
+    //     },
+    //     'ua': {
+    //         'title': '',
+    //         'description': ''
+    //     }
+    // },
     'window': {
         'en': {
             'title': '',
